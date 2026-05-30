06c5c77455f0a2fe35fed352af1b60c3bdb75fdc
manika2411 <manikasingla24@gmail.com>
2026-05-30 21:20:13 +0530

    added mongodb and authentication started

---
+++ backend/controllers/authController.js
@@ -0,0 +1,72 @@
+const User =
+  require('../models/User')
+
+const bcrypt =
+  require('bcryptjs')
+
+const jwt =
+  require('jsonwebtoken')
+
+const registerUser =
+  async (req, res) => {
+    try {
+      const {
+        name,
+        email,
+        password,
+      } = req.body
+
+      const exists =
+        await User.findOne({
+          email,
+        })
+
+      if (exists) {
+        return res
+          .status(400)
+          .json({
+            message:
+              'User already exists',
+          })
+      }
+
+      const hashedPassword =
+        await bcrypt.hash(
+          password,
+          10
+        )
+
+      const user =
+        await User.create({
+          name,
+          email,
+          password:
+            hashedPassword,
+        })
+
+      const token =
+        jwt.sign(
+          {
+            id: user._id,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: '7d',
+          }
+        )
+
+      res.status(201).json({
+        token,
+        user,
+      })
+    } catch (error) {
+      res.status(500).json({
+        message:
+          error.message,
+      })
+    }
+  }
+
+module.exports = {
+  registerUser,
+}
--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,21 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 
+const connectDB = require('./config/db')
+
+connectDB()
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
-
+const authRoutes = require('./routes/authRoutes')
+app.use('/api/auth',authRoutes)
 app.get('/', (req, res) => {
   res.send('Tripverse API Running')
 })
 
-const PORT =
-  process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  )
+  console.log(`Server running on ${PORT}`)
 })
